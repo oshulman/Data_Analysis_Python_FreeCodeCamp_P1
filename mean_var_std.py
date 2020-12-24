b36c58c1f64ef3af9a6ceee2343840cefc4ccbd4
oshulman <oshulman@umich.edu>
2020-12-24 00:38:25 -0500

    code fixed to be more efficient in employing numpy array capabilities

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 def calculate(lst):
-  # if input list is wrong size, raise value error with message
+    # if input list is wrong size, raise value error with message
   if len(lst) != 9:
     raise ValueError('List must contain nine numbers.')
 
+  # list turned into array
   flat = np.array(lst)
-  # creates array for axis2 data
+  # 1 x 9 array turned into 3 x 3 array
   arr = np.reshape(flat, (3, 3))
 
+  # dict created
   calc_dict = {}
 
   # declares lists
@@ -18,45 +20,42 @@ def calculate(lst):
   max_list = []
   min_list = []
   sum_list = []
-  
-  axis1_lists = []
 
-  # creates array for axis1 data
-  for row in range(3):
-    for col in range(3):
-      axis1_lists.append(arr[col][row])
-
-  arr_a_1 = np.reshape(axis1_lists, (3, 3))
-
-  # declares variables
+  # declares lists
+  # mean axes 1 and 2
   ma1 = []
   ma2 = []
+  # variance axes 1 and 2
   va1 = []
   va2 = []
+  # standard deviation axes 1 and 2
   sda1 = []
   sda2 = []
+  # maximimum axes 1 and 2
   max1 = []
   max2 = []
+  # minimum axes 1 and 2
   min1 = []
   min2 = []
+  # sum axes 1 and 2
   sum1 = []
   sum2 = []
 
-  # initializes variables
+  # iterates through elements of array to initialize above lists
   for i in range(3):
-    ma1.append(np.mean(arr_a_1[i]))
-    ma2.append(np.mean(arr[i]))
-    va1.append(np.var(arr_a_1[i]))
-    va2.append(np.var(arr[i]))
-    sda1.append(np.std(arr_a_1[i]))
-    sda2.append(np.std(arr[i]))
-    max1.append(np.max(arr_a_1[i]))
-    max2.append(np.max(arr[i]))
-    min1.append(np.min(arr_a_1[i]))
-    min2.append(np.min(arr[i]))
-    sum1.append(np.sum(arr_a_1[i]))
-    sum2.append(np.sum(arr[i]))
-
+    ma1.append(np.mean(arr[:, i]))
+    ma2.append(np.mean(arr[i, :]))
+    va1.append(np.var(arr[:, i]))
+    va2.append(np.var(arr[i, :]))
+    sda1.append(np.std(arr[:, i]))
+    sda2.append(np.std(arr[i, :]))
+    max1.append(np.max(arr[:, i]))
+    max2.append(np.max(arr[i, :]))
+    min1.append(np.min(arr[:, i]))
+    min2.append(np.min(arr[i, :]))
+    sum1.append(np.sum(arr[:, i]))
+    sum2.append(np.sum(arr[i, :]))
+  
   # adds variables to associated lists in format [axis1, axis2, flattened]
   mean_list.extend([ma1, ma2, np.mean(flat)])
   var_list.extend([va1,va2, np.var(flat)])
